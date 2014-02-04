936df7a0dcc63a379a85cbafbc64e80e9b11a31e
Francisco Trindade <frank.trindade@gmail.com>
2014-02-05 10:00:05 +1100

    added create and show actions to controller

--- app/controllers/spree/recurring_orders_controller.rb
+++ app/controllers/spree/recurring_orders_controller.rb
@@ -5,12 +5,18 @@ module Spree
  
     def create
       original_order = Spree::Order.find(params[:original_order_id])
-      recurring_order = Spree::RecurringOrder.new
-      recurring_order.original_order = original_order
+      @recurring_order = Spree::RecurringOrder.new
+      @recurring_order.original_order = original_order
 
-      if recurring_order.save
-        redirect_to("/recurring_orders/#{recurring_order.id}")
+      if @recurring_order.save
+        redirect_to("/recurring_orders/#{@recurring_order.id}")
+      else
+        render :new
       end
+    end
+
+    def show
+      @recurring_order = Spree::RecurringOrder.find(params[:id])
     end
 
   end
--- spec/controllers/spree/recurring_order_controller_spec.rb
+++ spec/controllers/spree/recurring_order_controller_spec.rb
@@ -3,32 +3,49 @@ require 'spec_helper'
 describe Spree::RecurringOrdersController do
   
   let(:original_order){ FactoryGirl.build(:order) }
-  let(:recurring_order){ double(Spree::RecurringOrder, save: true, id: 666) }
+  let(:recurring_order){ double(Spree::RecurringOrder, save: true, id: 666).as_null_object }
   let(:user) { mock_model Spree::User, :last_incomplete_spree_order => nil, :has_spree_role? => true, :spree_api_key => 'fake' }
 
   before :each do
     controller.stub :spree_current_user => user
+    controller.stub :check_authorization
   end
 
   describe 'create' do
 
-    it 'should create new recurring order' do
-      controller.stub :check_authorization
-
+    before :each do
       Spree::Order.stub(:find).with("1").and_return(original_order)
       Spree::RecurringOrder.stub(:new).and_return(recurring_order)
+    end
 
+    it 'should create new recurring order' do
       recurring_order.should_receive(:original_order=).with(original_order)
+      recurring_order.should_receive(:save).and_return(true)
 
       spree_post :create, original_order_id: 1
       response.should redirect_to("/recurring_orders/666")
+    end
+
+    it 'should render order complete if recurring order cant be saved' do
+      recurring_order.should_receive(:save).and_return(false)
+
+      spree_post :create, original_order_id: 1
+      response.should render_template(:new) 
     end
 
   end
 
   describe 'show' do
 
-    it 'should render'
+    before :each do
+      Spree::RecurringOrder.stub(:find).with("1").and_return(recurring_order)
+    end
+
+    it 'should render' do
+      spree_post :show, id: 1
+      response.should render_template(:show)
+    end
+
 
   end
 
