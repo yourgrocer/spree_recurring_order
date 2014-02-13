01c52fa2d3ee53719bd42c6dc5203e3c16606414
Francisco Trindade <frank.trindade@gmail.com>
2014-02-13 12:53:52 +1100

    added recurring message to checkout

---
+++ app/overrides/add_message_to_checkout.rb
@@ -0,0 +1,5 @@
+Deface::Override.new(:virtual_path => "spree/orders/edit",
+                     :name => "add_message_to_checkout",
+                     :insert_before => "[data-hook='outside_cart_form']",
+                     :partial => "spree/orders/recurring_message",
+                     :disabled => false)
