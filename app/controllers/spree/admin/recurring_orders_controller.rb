40a9454a0829504edf424b4e19fa17dcfd544cb9
Francisco Trindade <frank.trindade@gmail.com>
2015-01-31 13:49:04 +1100

    fixing issue with admin rendering

--- app/controllers/spree/admin/recurring_orders_controller.rb
+++ app/controllers/spree/admin/recurring_orders_controller.rb
@@ -3,7 +3,7 @@ module Spree
     class RecurringOrdersController < Spree::Admin::BaseController
 
       def index
-        @recurring_orders = Spree::RecurringOrder.all.select{|order| order.recurring_lists.empty? }
+        @recurring_orders = Spree::RecurringOrder.all.select{|order| !order.original_order.nil? }
         @recurring_orders_with_lists = Spree::RecurringOrder.all.select{|order| !order.recurring_lists.empty? }
       end
 
