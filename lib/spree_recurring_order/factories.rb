47bdb59f5e81dbfc68c33cdd6e9f19d055bb1e69
Francisco Trindade <frank.trindade@gmail.com>
2015-02-02 14:35:58 +1100

    Added mandatory fields to factory

--- lib/spree_recurring_order/factories.rb
+++ lib/spree_recurring_order/factories.rb
@@ -6,6 +6,8 @@ FactoryGirl.define do
 
   factory :recurring_list, class: Spree::RecurringList do
     user
+    timeslot '6am to 7:30am'
+    next_delivery_date Date.tomorrow
     after(:build) do |list|
       list.items << FactoryGirl.build(:recurring_list_item)
     end
