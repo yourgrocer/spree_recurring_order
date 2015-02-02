d9cb0eeef9583f1ade0ae103b1d7fd3d151a1093
Francisco Trindade <frank.trindade@gmail.com>
2015-02-02 13:58:20 +1100

    Removing useless test

--- spec/models/spree/recurring_list_spec.rb
+++ spec/models/spree/recurring_list_spec.rb
@@ -2,20 +2,6 @@ require 'spec_helper'
 
 describe Spree::RecurringOrder do
 
-  describe 'creation' do
-
-    it 'should have many list items' do
-      user = FactoryGirl.create(:user)
-      variant = FactoryGirl.create(:variant)
-
-      item = Spree::RecurringListItem.create(variant_id: variant.id, quantity: 1)
-      list = Spree::RecurringList.new(user: user)
-      list.items << item
-      list.save!
-    end
-
-  end
-
   describe 'validation' do
 
     it 'should require a user' do
