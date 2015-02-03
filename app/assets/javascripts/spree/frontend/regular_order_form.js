5a21515275854ef652276ba69eec1c213c3fa877
Francisco Trindade <frank.trindade@gmail.com>
2015-02-03 16:49:27 +1100

    moving js call to api controller

--- app/assets/javascripts/spree/frontend/regular_order_form.js
+++ app/assets/javascripts/spree/frontend/regular_order_form.js
@@ -3,10 +3,14 @@ function RegularOrderForm() {
   var that = this;
   this.initialize = function(){
     $("#recurring-order-form a.delete").click(function(){
-      var itemId = this.attributes["id"].value.replace("delete_","");
+      var ids = this.attributes["id"].value.replace("delete_","").split('_');
+      var listId = ids[0];
+      var itemId = ids[1];
+
       $.ajax({
-        url: 'recurring_list_items/' + itemId,
-        type: 'DELETE',
+        url: 'api/recurring_lists/' + itemId,
+        type: 'PUT',
+        data: {recurring_list_item: {id: itemId, destroy: true}},
         complete: function(result){
           window.location.reload(true);
         }
