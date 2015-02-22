0cfa02836606f861e8a03b59e368bf452f6c5b31
Francisco Trindade <frank.trindade@gmail.com>
2015-02-22 15:22:28 +1100

    happy case of creating order from base list working

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,6 @@
 Spree::Core::Engine.routes.draw do
 
-  resources :recurring_orders, only: [:create] do
-    resources :recurring_list_orders, only: [:create]
-  end
-
+  resources :recurring_orders, only: [:create] 
   resources :recurring_lists, only: [:create, :update]
   resources :recurring_list_items, only: [:destroy]
 
@@ -16,4 +13,10 @@ Spree::Core::Engine.routes.draw do
   get "admin/recurring_orders/", controller: 'admin/recurring_orders', action: 'index', as: 'admin_recurring_orders'
   get "admin/recurring_orders/:number", controller: 'admin/recurring_orders', action: 'show', as: 'admin_recurring_order'
   post "admin/recurring_emails/:number", controller: 'admin/recurring_emails', action: 'create', as: 'admin_recurring_email'
+
+  namespace :admin do
+    resources :recurring_orders, only: [:index] do
+      resources :recurring_list_orders, only: [:create]
+    end
+  end
 end
