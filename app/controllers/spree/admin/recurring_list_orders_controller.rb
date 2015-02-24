87a16bbc1b294be92d45910a4035347f09fe0e95
Francisco Trindade <frank.trindade@gmail.com>
2015-02-24 14:26:52 -0300

    refactoring

--- app/controllers/spree/admin/recurring_list_orders_controller.rb
+++ app/controllers/spree/admin/recurring_list_orders_controller.rb
@@ -5,37 +5,32 @@ module Spree
       def create
         @recurring_order = Spree::RecurringOrder.find(params[:recurring_order_id])
 
-        if base_list 
-          order_to_merge = base_list.user.last_incomplete_spree_order
-          if order_to_merge && order_to_merge.delivery_date
-            fail_with_message('User has already an existing incomplete order with delivery date set')
-          else
-            @order = Order.create
-            @order.recurring_order = @recurring_order
-            @order.email = base_list.user.email
-            @order.created_by = base_list.user
-            @order.user = base_list.user
+        if base_list.nil?
+          fail_with_message('Recurring order does not have a base list')
+        elsif order_to_merge && order_to_merge.delivery_date
+          fail_with_message('User has already an existing incomplete order with delivery date set')
+        else
+          @order = Order.create
+          @order.recurring_order = @recurring_order
+          @order.email = base_list.user.email
+          @order.created_by = base_list.user
+          @order.user = base_list.user
 
-            @order.ship_address = ship_address(base_list) 
-            @order.bill_address = bill_address(base_list) 
+          @order.ship_address = ship_address(base_list) 
+          @order.bill_address = bill_address(base_list) 
 
-            set_extended_values
+          set_extended_values
 
-            @order.save!
+          @order.save!
 
-            @order.merge!(order_to_merge) if order_to_merge
+          @order.merge!(order_to_merge) if order_to_merge
 
-            order_contents = Spree::OrderContents.new(@order)
-            base_list.items.each do |item|
-              order_contents.add(item.variant, item.quantity)
-            end
-
-            redirect_to(edit_admin_order_url(@order.number))
+          order_contents = Spree::OrderContents.new(@order)
+          base_list.items.each do |item|
+            order_contents.add(item.variant, item.quantity)
           end
 
-        else
-          flash[:error] = 'Order creation failed - recurring order doesnt have a base list'
-          redirect_to(admin_recurring_order_url(@recurring_order.number))
+          redirect_to(edit_admin_order_url(@order.number))
         end
       end
 
@@ -60,6 +55,11 @@ module Spree
 
       def base_list
         @recurring_order.base_list
+      end
+
+      def order_to_merge
+        @order_to_merge ||= base_list.nil? ? nil : base_list.user.last_incomplete_spree_order
+        @order_to_merge
       end
 
     end
