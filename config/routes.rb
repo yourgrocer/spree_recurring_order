4c634d124c0c621ad5b3d68219300665c8ce0a32
Igor Svilenkov <igor@svilenkov.com>
2015-04-24 15:47:13 +0200

    addd recurring order update routes

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,7 @@ Spree::Core::Engine.routes.draw do
 
 
   get "/recurring_orders/:id", controller: 'recurring_orders', action: 'show', as: 'recurring_order'
+  patch "/recurring_orders/:id", controller: 'recurring_orders', action: 'update', as: 'recurring_order_put'
   get "admin/recurring_orders/", controller: 'admin/recurring_orders', action: 'index', as: 'admin_recurring_orders'
   get "admin/recurring_orders/:number", controller: 'admin/recurring_orders', action: 'show', as: 'admin_recurring_order'
   post "admin/recurring_emails/:number", controller: 'admin/recurring_emails', action: 'create', as: 'admin_recurring_email'
