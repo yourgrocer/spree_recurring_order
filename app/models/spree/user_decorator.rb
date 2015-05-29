f1f5b264dc2a927208cf4fdd2943e64e87a7cbe8
Francisco Trindade <frank.trindade@gmail.com>
2015-05-29 15:41:28 +1000

    added has regular active order to user

--- app/models/spree/user_decorator.rb
+++ app/models/spree/user_decorator.rb
@@ -1,6 +1,10 @@
 Spree::User.class_eval do
   has_one :base_list, class_name: "Spree::RecurringList"
 
+  def has_active_regular_order?
+    self.base_list.recurring_order.active? rescue false
+  end
+
   def has_incomplete_order_booked?
     self.last_incomplete_spree_order && self.last_incomplete_spree_order.delivery_date
   end
