eb4aa468fa4af379d50ce4cd853fb583b60dc08e
Steph Hof <steph.hof@gmail.com>
2015-10-09 10:52:42 +1100

    Disabling override for recurring message in cart

--- app/overrides/add_message_to_checkout.rb
+++ app/overrides/add_message_to_checkout.rb
@@ -2,4 +2,4 @@ Deface::Override.new(:virtual_path => "spree/orders/edit",
                      :name => "add_message_to_checkout",
                      :insert_before => "[data-hook='outside_cart_form']",
                      :partial => "spree/orders/recurring_message",
-                     :disabled => false)
+                     :disabled => true)
