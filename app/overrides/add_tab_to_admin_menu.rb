aaaacc20c22859348c32f782d7ca7e970b30b707
Steph Hof <steph.hof@gmail.com>
2017-08-09 18:47:09 +1000

    Started to fix up the partials for spree 3

--- app/overrides/add_tab_to_admin_menu.rb
+++ app/overrides/add_tab_to_admin_menu.rb
@@ -1,5 +1,5 @@
-Deface::Override.new(:virtual_path => "spree/admin/shared/_menu",
+Deface::Override.new(:virtual_path => "spree/layouts/admin",
                      :name => "add_tab_to_admin_menu",
-                     :insert_top => "[data-hook='admin_tabs']",
+                     :insert_top => "#main-sidebar",
                      :partial => "spree/admin/shared/recurring_orders_tab",
                      :disabled => false)
